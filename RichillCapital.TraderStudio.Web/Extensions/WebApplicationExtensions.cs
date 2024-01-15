ed43bb08e858401b8b57f65d04ab9aa1f7bcc7e0
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-01-15 23:00:25 +0800

    update rules

--- RichillCapital.TraderStudio.Web/Extensions/WebApplicationExtensions.cs
+++ RichillCapital.TraderStudio.Web/Extensions/WebApplicationExtensions.cs
@@ -28,6 +28,7 @@ public static class WebApplicationExtensions
         else
         {
             app.UseExceptionHandler("/Error", createScopeForErrors: true);
+
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
