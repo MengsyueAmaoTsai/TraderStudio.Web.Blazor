1f53cf78f1817d43acb1f2e8030edc403011f7be
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-05-09 23:26:57 +0800

    add identity

---
+++ Src/Identity/IdentityExtensions.cs
@@ -0,0 +1,14 @@
+namespace RichillCapital.TraderStudio.Web.Identity;
+
+internal static class IdentityExtensions
+{
+    internal static IServiceCollection AddIdentity(this IServiceCollection services)
+    {
+        return services;
+    }
+}
+
+internal sealed record IdentityOptions
+{
+    internal const string SectionKey = "Identity";
+}
