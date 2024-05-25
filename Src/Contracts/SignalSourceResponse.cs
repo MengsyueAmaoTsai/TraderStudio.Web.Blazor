ffd317cb29607d5004ca98b51cbad6e1695246fb
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-05-25 16:19:20 +0800

    Update signal source details page

--- Src/Contracts/SignalSourceResponse.cs
+++ Src/Contracts/SignalSourceResponse.cs
@@ -5,11 +5,21 @@ public record SignalSourceResponse
     public required string Id { get; init; }
     public required string Name { get; init; }
     public required string Description { get; init; }
-    public required decimal WinRate { get; init; }
-    public required decimal Returns { get; init; }
 }
 
 public record SignalSourceDetailsResponse : SignalSourceResponse
 {
+    public required IEnumerable<SignalResponse> Signals { get; init; }
+}
 
+
+public sealed record SignalResponse
+{
+    public required string Id { get; init; }
+    public required DateTimeOffset Time { get; init; }
+    public required string SourceId { get; init; }
+    public required string TradeType { get; init; }
+    public required string Symbol { get; init; }
+    public required decimal Quantity { get; init; }
+    public required decimal Price { get; init; }
 }
