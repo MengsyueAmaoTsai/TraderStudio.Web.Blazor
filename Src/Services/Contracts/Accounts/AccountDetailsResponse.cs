76d18b38dad303f24ad33a87cfb64f54e30962ca
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-05-31 22:44:09 +0800

    list account executions

--- Src/Services/Contracts/Accounts/AccountDetailsResponse.cs
+++ Src/Services/Contracts/Accounts/AccountDetailsResponse.cs
@@ -1,3 +1,4 @@
+using RichillCapital.TraderStudio.Web.Services.Contracts.Executions;
 using RichillCapital.TraderStudio.Web.Services.Contracts.Orders;
 
 namespace RichillCapital.TraderStudio.Web.Services.Contracts.Accounts;
@@ -5,4 +6,5 @@ namespace RichillCapital.TraderStudio.Web.Services.Contracts.Accounts;
 public sealed record AccountDetailsResponse : AccountResponse
 {
     public required IEnumerable<OrderResponse> Orders { get; init; }
+    public required IEnumerable<ExecutionResponse> Executions { get; init; }
 }
