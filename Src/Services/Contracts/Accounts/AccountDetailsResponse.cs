2250fd7f0492b81ea3753b7feba1b3ac84a4a1aa
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-05-31 20:21:43 +0800

    add order table to account details page

--- Src/Services/Contracts/Accounts/AccountDetailsResponse.cs
+++ Src/Services/Contracts/Accounts/AccountDetailsResponse.cs
@@ -1,6 +1,8 @@
+using RichillCapital.TraderStudio.Web.Services.Contracts.Orders;
+
 namespace RichillCapital.TraderStudio.Web.Services.Contracts.Accounts;
 
 public sealed record AccountDetailsResponse : AccountResponse
 {
-
+    public required IEnumerable<OrderResponse> Orders { get; init; }
 }
---
+++ Src/Services/Contracts/Orders/OrderResponse.cs
@@ -0,0 +1,20 @@
+namespace RichillCapital.TraderStudio.Web.Services.Contracts.Orders;
+
+public record OrderResponse
+{
+    public required string Id { get; init; }
+
+    public required string AccountId { get; init; }
+
+    public required string TradeType { get; init; }
+
+    public required string Type { get; init; }
+
+    public required string Symbol { get; init; }
+
+    public required decimal Quantity { get; init; }
+
+    public required decimal Price { get; init; }
+
+    public required string TimeInForce { get; init; }
+}
