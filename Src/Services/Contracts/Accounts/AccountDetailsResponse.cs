8a82761cf49ba3d3c87249316618fab9bebf8319
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-05-31 23:16:50 +0800

    list account positions

--- Src/Services/Contracts/Accounts/AccountDetailsResponse.cs
+++ Src/Services/Contracts/Accounts/AccountDetailsResponse.cs
@@ -1,10 +1,16 @@
 using RichillCapital.TraderStudio.Web.Services.Contracts.Executions;
 using RichillCapital.TraderStudio.Web.Services.Contracts.Orders;
+using RichillCapital.TraderStudio.Web.Services.Contracts.Positions;
 
 namespace RichillCapital.TraderStudio.Web.Services.Contracts.Accounts;
 
-public sealed record AccountDetailsResponse : AccountResponse
+
+public sealed record AccountDetailsResponse :
+    AccountResponse
 {
     public required IEnumerable<OrderResponse> Orders { get; init; }
+
     public required IEnumerable<ExecutionResponse> Executions { get; init; }
+
+    public required IEnumerable<PositionResponse> Positions { get; init; }
 }
