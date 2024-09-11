872be4a28b22301d93bbf9357fdfbd89f992185e
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-11 19:16:29 +0800

    add scheme

--- Libs/RichillCapital.Infrastructure/Identity/CustomAuthenticationSchemes.cs
+++ Libs/RichillCapital.Infrastructure/Identity/CustomAuthenticationSchemes.cs
@@ -4,4 +4,5 @@ internal static class CustomAuthenticationSchemes
 {
     internal const string CookieDefault = "RichillCapital.Cookie";
     internal const string OpenIdConnectDefault = "RichillCapital.OpenIdConnect";
+    internal const string JwtBearerDefault = "RichillCapital.JwtBearer";
 }
